65efad53c13da19270ce281639f56ba339d1ab18
Dan Anderson <danfanderson@gmail.com>
2020-08-24 18:30:55 -0500

    define flask routes

--- python/app.py
+++ python/app.py
@@ -1,7 +1,15 @@
+# Modeling imports  
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 import pandas as pd
+
+# Flask imports
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
 
 # class2int returns 1 for a 'spam' class; 0 otherwise
 def class2int(val):
@@ -22,7 +30,6 @@ def read_wrangle_csv(fname):
 # gen_model generates a vectorizer and model object and fits 
 #   a model given the dataset processed at startup
 def gen_model(DF):
-    print(DF.describe())
     X = DF['Message']
     y = DF['is_spam']
 
@@ -56,10 +63,50 @@ def mdl_pred(mdl, vtzr, val):
 df = read_wrangle_csv("../data/standardSpamData.csv")
 # Generate and fit a Bayesian Classifier model
 model, vctrizr  = gen_model(df)
-# Generate a model prediction
-pred = mdl_pred(
-    model,
-    vctrizr,
-    ["Even my brother is not like to speak with me. They treat me like aids patent."])
-print("pred is: ", pred)
+
+# --- FLASK APP ---
+# /status can be used to verify that the Flask app is running and responding
+@app.route('/status')
+def test():
+  global df
+  num_rows = len(df.index)
+  ret_dict = {}
+  ret_dict["msg"] = "app.py is up and running"
+  ret_dict["note"] = "the number of rows in the dataset is: " + str(num_rows)
+
+  return jsonify(ret_dict)
+
+# /predict accepts a string of text and returns a spam prediction value
+@app.route('/predict', methods = ['POST'])
+def predict():
+  global model
+  global vctrizr
+
+  # Get the request's body as json
+  jsn      = request.get_json(force=True)
+  ret_dict = {}
+
+  # Validate the inbound json body parameters
+  tmp_string = jsn["text"]
+  if len(tmp_string) == 0:
+    # missing text value
+    ret_dict["msg"] = "missing text value"
+    return jsonify(ret_dict), 400
+
+  # Process the inbound text and generate a list of tokens
+  tmp_pred = mdl_pred(model, vctrizr, [tmp_string])
+
+  # Construct the return object
+  return_object = {}
+  return_object["msg"]              = "your prediction"
+  return_object["content"]          = dict(pred=float(tmp_pred[0]))
+  return jsonify(return_object)
+
+# Start the flask app
+if __name__ == '__main__':
+  app_domain = 'localhost'
+  app_port   = 8091
+  print(f'INFO: starting web api server on {app_domain}:{app_port}')
+  app.run(host='localhost', port=app_port)
+
 
