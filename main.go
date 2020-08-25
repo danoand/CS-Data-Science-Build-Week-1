a8c2247efdf0059bc6670f790b764877cf5a78ed
Dan Anderson <danfanderson@gmail.com>
2020-08-24 19:37:03 -0500

    interim commit

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"regexp"
+
+	"github.com/davecgh/go-spew/spew"
 
 	"github.com/danoand/utils"
 	"github.com/gin-gonic/gin"
@@ -12,6 +17,7 @@ import (
 
 const k = .5
 const stdMdlDataFname = "data/standardSpamData.csv"
+const URLPyMLSvc = "http://localhost:8091/predict"
 
 var err error
 var rgxAlphaNum = regexp.MustCompile("[^a-z0-9]")
@@ -33,9 +39,9 @@ func getModelInfo(c *gin.Context) {
 	c.JSON(http.StatusOK, respMap)
 }
 
-// handlerPred handles a request to generate a spam prediction for a
+// hdlrPred handles a request to generate a spam prediction for a
 //     given string of text
-func handlerPred(c *gin.Context) {
+func hdlrPred(c *gin.Context) {
 	var err error
 	var bbytes []byte
 	var prms = make(map[string]string)
@@ -101,6 +107,120 @@ func handlerPred(c *gin.Context) {
 	c.JSON(http.StatusOK, respMap)
 }
 
+// hdlrPyPred handles a request to generate a prediction using Python libraries
+func hdlrPyPred(c *gin.Context) {
+	var (
+		err             error
+		bbytes, reqBody []byte
+		rspBody         []byte
+		resp            *http.Response
+		prms            = make(map[string]string)
+		respMap         = make(map[string]interface{})
+		respSvc         = make(map[string]float32)
+		cMap            = make(map[string]interface{})
+	)
+
+	// Grab the request body data
+	bbytes, err = c.GetRawData()
+	if err != nil {
+		// error fetching the request body
+		log.Printf("ERROR: %v - error fetching the request body. See: %v\n",
+			utils.FileLine(),
+			err)
+		respMap["msg"] = "an error occurred, please try again"
+		cMap["havepred"] = false
+		respMap["content"] = cMap
+		c.JSON(http.StatusInternalServerError, respMap)
+		return
+	}
+
+	// Parse the request body
+	err = utils.FromJSONBytes(bbytes, &prms)
+	if err != nil {
+		// error parsing the request body data
+		log.Printf("ERROR: %v - error parsing the request body data. See: %v\n",
+			utils.FileLine(),
+			err)
+		respMap["msg"] = "an error occurred, please try again"
+		cMap["havepred"] = false
+		respMap["content"] = cMap
+		c.JSON(http.StatusInternalServerError, respMap)
+		return
+	}
+
+	// Missing text value?
+	if len(prms["text"]) == 0 {
+		// error missing text value
+		log.Printf("ERROR: %v - error missing text value\n",
+			utils.FileLine())
+		respMap["msg"] = "error missing text value"
+		cMap["havepred"] = false
+		respMap["content"] = cMap
+		c.JSON(http.StatusBadRequest, respMap)
+		return
+	}
+
+	// Construct the http service request
+	reqBody, err = json.Marshal(map[string]string{
+		"text": prms["text"],
+	})
+	if err != nil {
+		// error constructing an http service request body
+		log.Printf("ERROR: %v - error constructing an http service request body. See: %v\n",
+			utils.FileLine(),
+			err)
+		respMap["msg"] = "an error occurred, please try again"
+		cMap["havepred"] = false
+		respMap["content"] = cMap
+		c.JSON(http.StatusInternalServerError, respMap)
+		return
+	}
+
+	resp, err = http.Post(URLPyMLSvc, "application/json", bytes.NewReader(reqBody))
+	if err != nil {
+		// error calling the http service
+		log.Printf("ERROR: %v - error calling the http service. See: %v\n",
+			utils.FileLine(),
+			err)
+		respMap["msg"] = "an error occurred, please try again"
+		cMap["havepred"] = false
+		respMap["content"] = cMap
+		c.JSON(http.StatusInternalServerError, respMap)
+		return
+	}
+	defer resp.Body.Close()
+
+	rspBody, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// error reading the data from the http service response
+		log.Printf("ERROR: %v - error reading the data from the http service response. See: %v\n",
+			utils.FileLine(),
+			err)
+		respMap["msg"] = "an error occurred, please try again"
+		cMap["havepred"] = false
+		respMap["content"] = cMap
+		c.JSON(http.StatusInternalServerError, respMap)
+		return
+	}
+
+	err = utils.FromJSONBytes(rspBody, &respSvc)
+	if err != nil {
+		// error parsing the data from the http service response
+		log.Printf("ERROR: %v - error parsing the data from the http service response. See: %v\n",
+			utils.FileLine(),
+			err)
+		respMap["msg"] = "an error occurred, please try again"
+		cMap["havepred"] = false
+		respMap["content"] = cMap
+		c.JSON(http.StatusInternalServerError, respMap)
+		return
+	}
+
+	spew.Dump(respSvc)
+}
+
+//
+
 func main() {
 	stdModel, err = fitSpamModel2Data("data/standardSpamData.csv")
 	if err != nil {
@@ -128,7 +248,8 @@ func main() {
 	r.StaticFile("/index.html", "./www/index.html")
 	r.StaticFile("/index.htm", "./www/index.html")
 	r.StaticFile("/favicon.ico", "./www/favicon.ico")
-	r.POST("/getModelPred", handlerPred)
+	r.POST("/getModelPred", hdlrPred)
+	r.POST("/getPyModelPred", hdlrPyPred)
 	r.GET("/getModelInfo", getModelInfo)
 
 	// Start the web server
